e7d3358dad341c4faccc5a3355479d4f01f4b817
Tushaar Rohatgi <hitushaar@gmail.com>
2025-01-02 20:04:41 +0530

    added responsiveness in the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Close from "./Close";
 import Menu from "./Menu";
-import { useSelector, useDispatch } from "react-redux";
-import { Popover, PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
-// import { logout } from "../store/authSlice"; // Import your logout action
+import { useSelector } from "react-redux";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@radix-ui/react-popover";
 
 export default function Navbar() {
   const { user } = useSelector((store) => store.auth);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
   const location = useLocation();
 
   const isLoginOrSignupPage =
@@ -27,129 +28,117 @@ export default function Navbar() {
     setBtn(!btn);
   };
 
-  // const handleLogout = () => {
-  //   // Dispatch logout action to clear user data
-  //   dispatch(logout());
-  //   // Redirect user to the login page
-  //   navigate("/login");
-  // };
-
   return (
-    <>
-      <div className="lg:h-[65px] h-[55px] bg-[#31a022] flex items-center">
-        <nav className="flex justify-between items-center w-full px-4 lg:px-10">
-          {/* Brand Name */}
-          <div className="text-[30px] lg:text-3xl font-Ubun text-black">
-            Field Finder
-          </div>
+    <div className="bg-[#31a022] flex flex-row items-center justify-between p-4 relative z-50">
+      {/* Brand Name */}
+      <div className="text-[24px] lg:text-[30px] font-bold text-white">
+        <Link to="/">Field Finder</Link>
+      </div>
 
-          {/* Desktop Links */}
-          <div className="hidden lg:flex lg:gap-x-8 lg:text-[22px]">
-            {links.map((val) => (
-              <ul key={val.id}>
-                <li className="relative group">
-                  <Link to={val.link} className="hover:text-gray-900 text-black">
-                    {val.name}
-                  </Link>
-                  <span className="absolute left-0 bottom-0 w-full h-[2px] bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+      {/* Desktop Links */}
+      <div className="hidden lg:flex lg:items-center lg:space-x-8 lg:justify-center lg:flex-1">
+        {links.map((link) => (
+          <Link
+            key={link.id}
+            to={link.link}
+            className="text-white hover:text-gray-300 text-lg"
+          >
+            {link.name}
+          </Link>
+        ))}
+      </div>
+
+      {/* Login/Profile Section */}
+      <div className="hidden lg:flex lg:items-center relative z-50">
+        {!user ? (
+          !isLoginOrSignupPage && (
+            <Link
+              to="/login"
+              className="bg-black text-white px-4 py-2 rounded-full text-center hover:bg-gray-800 transition"
+            >
+              Login
+            </Link>
+          )
+        ) : (
+          <Popover>
+            <PopoverTrigger asChild>
+              <div className="flex items-center space-x-2 cursor-pointer">
+                <span className="text-white font-medium">
+                  Hello, {user.fullname}
+                </span>
+                <img
+                  src="./images/profileImage.jpg"
+                  alt="Profile"
+                  className="w-8 h-8 rounded-full object-cover"
+                />
+              </div>
+            </PopoverTrigger>
+            <PopoverContent
+              align="end"
+              className="p-4 bg-white rounded-lg shadow-md border border-gray-300 z-50"
+            >
+              <ul className="flex flex-col space-y-2 text-gray-700">
+                <li className="hover:bg-gray-100 py-2 px-4 rounded-md cursor-pointer">
+                  <Link to="/userprofile">Profile</Link>
+                </li>
+                <li className="hover:bg-gray-100 py-2 px-4 rounded-md cursor-pointer">
+                  Logout
                 </li>
               </ul>
-            ))}
-          </div>
-
-          {/* Desktop Greeting and Profile */}
-          <div className="hidden lg:flex lg:items-center">
-            {!user ? (
-              !isLoginOrSignupPage && (
-                <Link to="/login">
-                  <button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-full transition-colors duration-300">
-                    Login
-                  </button>
-                </Link>
-              )
-            ) : (
-              <Popover>
-                <PopoverTrigger>
-                  <div className="flex items-center cursor-pointer">
-                    <span className="text-black font-bold mr-4">
-                      Hello, {user.fullname}
-                    </span>
-                    <img
-                      src="./images/profileImage.jpg"
-                      alt="Profile"
-                      className="w-[40px] h-[40px] rounded-full object-cover"
-                    />
-                  </div>
-                </PopoverTrigger>
-                <PopoverContent
-                  align="end"
-                  className="p-2 bg-white rounded-lg shadow-lg border border-gray-200"
-                >
-                  <ul className="flex flex-col text-sm">
-                    <li className="py-2 px-4 hover:bg-gray-100 cursor-pointer">
-                      <Link to="/userprofile">Profile</Link>
-                    </li>
-                    <li
-                      className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
-                      
-                    >
-                      Logout
-                    </li>
-                  </ul>
-                </PopoverContent>
-              </Popover>
-            )}
-          </div>
-
-          {/* Mobile Toggle Button */}
-          <div className="lg:hidden flex items-center">
-            <button className="h-[25px] w-[25px]" onClick={toggleBtn}>
-              {btn ? <Close /> : <Menu />}
-            </button>
-          </div>
-        </nav>
-
-        {/* Mobile Menu */}
-        <div
-          className={`transition-all duration-500 ease-in-out ${
-            btn ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
-          } overflow-hidden lg:hidden`}
-        >
-          <div className="bg-white opacity-95">
-            <ul className="text-2xl mt-[20px] pt-[20px] text-black w-screen flex flex-col align-center items-center h-screen">
-              {links.map((val) => (
-                <li key={val.id} className="hover:text-[#576CBC] my-[15px]">
-                  <Link to={val.link} onClick={toggleBtn}>
-                    {val.name}
-                  </Link>
-                </li>
-              ))}
-
-              {/* Mobile Greeting and Profile */}
-              {!user ? (
-                !isLoginOrSignupPage && (
-                  <li className="hover:text-[#576CBC] my-[15px]">
-                    <Link to="/login" onClick={toggleBtn}>
-                      Login
-                    </Link>
-                  </li>
-                )
-              ) : (
-                <div className="flex flex-col items-center">
-                  <p className="text-black font-bold mb-2">
-                    Hello, {user.fullname}
-                  </p>
-                  <img
-                    src="./images/profileImage.jpg"
-                    alt="Profile"
-                    className="w-[40px] h-[40px] rounded-full object-cover"
-                  />
-                </div>
-              )}
-            </ul>
-          </div>
-        </div>
+            </PopoverContent>
+          </Popover>
+        )}
       </div>
-    </>
+
+      {/* Mobile Toggle Button */}
+      <div className="lg:hidden flex">
+        <button
+          onClick={toggleBtn}
+          className="text-white"
+          style={{ width: "25px", height: "25px" }}
+        >
+          {btn ? <Close /> : <Menu />}
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`lg:hidden absolute top-[60px] left-0 w-full ${
+          btn ? "block" : "hidden"
+        } bg-black text-white p-4 space-y-4 z-50`}
+      >
+        {links.map((link) => (
+          <Link
+            key={link.id}
+            to={link.link}
+            onClick={toggleBtn}
+            className="block hover:text-gray-300 text-lg"
+          >
+            {link.name}
+          </Link>
+        ))}
+
+        {!user ? (
+          !isLoginOrSignupPage && (
+            <Link
+              to="/login"
+              onClick={toggleBtn}
+              className="block bg-gray-700 text-white px-4 py-2 rounded-full text-center hover:bg-gray-800 transition"
+            >
+              Login
+            </Link>
+          )
+        ) : (
+          <div className="flex flex-col items-center space-y-2">
+            <span className="text-white">Hello, {user.fullname}</span>
+            <img
+              src="./images/profileImage.jpg"
+              alt="Profile"
+              className="w-8 h-8 rounded-full object-cover"
+            />
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
